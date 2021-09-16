602bab3e0e65aa6ce75f8a6e1d7e6d54d65bb672
Deepak Goyal <dg887180@gmail.com>
2021-09-16 22:50:57 +0530

    update

--- app.js
+++ app.js
@@ -12,10 +12,10 @@ calculateBtn.addEventListener("click",()=>{
     }else{
 
         if(year%4==0 && year%100 !=0 || year%400==0){
-            outputDiv.innerText="Congratulation! "+ inputTextValue + " your birthday is comming in leap year";
+            outputDiv.innerText="Congratulation! "+ inputTextValue + " your birthday is coming in leap year";
             outputDiv.style.color="green";
         }else{
-            outputDiv.innerText="Sorry! " + inputTextValue +" your birthday is not comming in leap year";
+            outputDiv.innerText="Sorry! " + inputTextValue +" your birthday is not coming in leap year";
             outputDiv.style.color="red";
         }
     }
